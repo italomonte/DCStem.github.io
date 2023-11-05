142ccfa5416b9a388563ab5800586f187b5e0d23
Italo Monte <italopessoal@outlook.com>
2023-11-04 23:35:15 -0300

    urls

--- data_academy/urls.py
+++ data_academy/urls.py
@@ -4,9 +4,7 @@ from . import views
 app_name = 'data_academy'
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('abp/', views.abp, name="abp"),
+    path('', views.home, name="inicio"),
+    path('conteudos/', views.conteudos, name="conteudos"),
     path('login/', views.login, name="login"),
-    path('estatistica/', views.estatistica_python, name="estatistica"),
-    path('problema/', views.problema, name="problema"),
 ]
--- data_academy/views.py
+++ data_academy/views.py
@@ -6,15 +6,9 @@ from django.shortcuts import render, HttpResponse
 def home (request):
     return render(request, 'data_academy/pages/home.html')
 
-def abp (request):
+def conteudos (request):
     return render(request, 'data_academy/pages/abp.html')
-
 
 def login (request):
     return render(request, 'data_academy/pages/login.html')
 
-def estatistica_python (request):
-    return render(request, 'data_academy/pages/estatisticapython.html')
-
-def problema (request):
-    return render(request, 'data_academy/pages/problema.html')
