52c25d45524f51eaa931d4cbd6c2f377b5fc3465
Italo Monte <italopessoal@outlook.com>
2023-11-18 01:37:37 -0300

    update js

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^%qmqkvtgcurev)zw^soj6gkuolxk#l%3n9py-u9251x+km!0w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['italomonte.pythonanywhere.com']
+ALLOWED_HOSTS = ['italomonte.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
