e770a1dee509e19925dc1a431c0c973a1f837995
Italo Monte <italopessoal@outlook.com>
2023-11-18 01:33:33 -0300

    static to js

--- data_academy/static/data_academy/js/main.js
+++ data_academy/static/data_academy/js/main.js
@@ -5,3 +5,69 @@ ace.edit(editor, {
     mode: 'ace/mode/python',
 })
 
+let currentSlide = 0;
+    const slides = document.querySelectorAll('.slide');
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            if (i === index) {
+                slide.style.display = 'inline-block';
+                slide.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            } else {
+                slide.style.display = 'none';
+            }
+        });
+    }
+    
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % slides.length;
+
+        showSlide(currentSlide);
+        console.log("foi");
+    }
+    
+    function previousSlide() {
+        currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+        showSlide(currentSlide);
+        console.log("voltou");
+    }
+    
+
+    document.addEventListener('DOMContentLoaded', function () {
+        showSlide(currentSlide);
+        document.getElementById('nextSlideButton').addEventListener('click', nextSlide);
+        document.getElementById('previousSlideButton').addEventListener('click', previousSlide);
+        
+        new DataTable('#example');
+        var form = document.querySelector('form');
+        var outputElement = document.getElementById('output');
+        var editor = ace.edit('editora');
+        outputElement.readOnly = true;
+
+
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            var code = editor.getValue();
+            var formData = new FormData(form);
+            formData.append('code', code);
+
+            outputElement.textContent = '';
+
+            fetch(form.action, {
+                method: 'POST',
+                body: formData,
+            })
+                .then(response => response.json())
+                .then(data => {
+
+                    outputElement.textContent = data.output;
+
+                })
+                .catch(error => {
+                    console.error('Erro:', error);
+                    outputElement.textContent = 'Ocorreu um erro ao executar o código.';
+
+                });
+        });
+    });
