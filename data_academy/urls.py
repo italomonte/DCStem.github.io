fd8d60017e2142c887bb062b6618e3f2e965ef43
Italo Monte <italopessoal@outlook.com>
2023-11-19 20:54:23 -0300

    .

--- data_academy/urls.py
+++ data_academy/urls.py
@@ -5,8 +5,7 @@ app_name = 'data_academy'
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('content/', views.content, name="content"),
+    path('content_road/', views.content_road, name="content_road"),
     path('login/', views.login, name="login"),
     path('task/', views.task, name="task"),
-    path('content_road/', views.content_road, name="content_road"),
 ]
