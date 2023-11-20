f38c86ab10197ef2780cc726222a7a7040f5b70f
Italo Monte <italopessoal@outlook.com>
2023-11-20 17:08:45 -0300

    .

--- data_academy/views.py
+++ data_academy/views.py
@@ -30,8 +30,8 @@ def task(request):
         # Crie um objeto StringIO para coletar a saída
         output = io.StringIO()
         sys.stdout = output  # Redirecione a saída padrão
-        df = pd.read_csv("/home/italomonte/Data_Academy/data_academy/static/data_academy/csv/BD_Refrigerante.csv")
-        #df = pd.read_csv("data_academy/static/data_academy/csv/BD_Refrigerante.csv")
+        #df = pd.read_csv("/home/italomonte/Data_Academy/data_academy/static/data_academy/csv/BD_Refrigerante.csv")
+        df = pd.read_csv("data_academy/static/data_academy/csv/BD_Refrigerante.csv")
         pd.set_option('display.max_rows', None)
         pd.set_option('display.max_columns', None)
         pd.set_option('display.width', None)
@@ -45,8 +45,8 @@ def task(request):
         sys.stdout = sys.__stdout__
         responseData = {'code': code, 'output': output.getvalue()}
         return JsonResponse(responseData)
-    df = pd.read_csv("/home/italomonte/Data_Academy/data_academy/static/data_academy/csv/BD_Refrigerante.csv")
-    #df = pd.read_csv("data_academy/static/data_academy/csv/BD_Refrigerante.csv")
+    #df = pd.read_csv("/home/italomonte/Data_Academy/data_academy/static/data_academy/csv/BD_Refrigerante.csv")
+    df = pd.read_csv("data_academy/static/data_academy/csv/BD_Refrigerante.csv")
     html_table = df.to_html().replace("<table", '<table id="example" class="table table-striped" style="width:100%"')
 
     return render(request, 'data_academy/pages/task.html', {"table": html_table})
